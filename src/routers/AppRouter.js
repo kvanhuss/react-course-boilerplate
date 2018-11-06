23929c10f09db8b1082dd9a0d1a92a9fb95e5cdd
ko08125 <vanhusskathryn@johndeere.com>
2018-11-05 22:02:43 -0600

    Init commit

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Router, Route, Switch, Link, NavLink } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
+import DashboardPage from '../components/DashboardPage';
+import NotFoundPage from '../components/NotFoundPage';
+import LoginPage from '../components/LoginPage';
+import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
+
+// BrowserRouter expects to have no more than 1 child
+// Switch stops going through routes once there's a match so 404 won't show on every page
+
+export const history = createHistory();
+
+const AppRouter = () => (
+    <Router history={history}>
+        <div>
+            <Switch>
+                <PublicRoute path="/" component={LoginPage} exact={true} />
+                <PrivateRoute path="/dashboard" component={DashboardPage} />
+                <Route component={NotFoundPage} />
+            </Switch>
+        </div>
+    </Router>
+);
+
+export default AppRouter;
